8bab7d783f478f23ee1592dbf266fee8e3b6ce53
Rick Slotboom <rick.slotboom@hotmail.com>
2026-05-01 13:21:04 +0200

    engelse menu

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
- 
+import { Link } from "react-router-dom";
+
 export default function Menu() {
   const menuImages = [
     { src: "/images/menuvoorblad.jpg", alt: "Menu voorblad – De Speciaalzaak" },
@@ -7,9 +8,27 @@ export default function Menu() {
     { src: "/images/menudranken.jpg",  alt: "Dranken menukaart – De Speciaalzaak" },
     { src: "/images/menuborrel.jpg",   alt: "Borrel menukaart – De Speciaalzaak" },
   ];
- 
+
   return (
     <div style={{ width: "100%", maxWidth: "1200px", margin: "0 auto" }}>
+
+      {/* English menu banner */}
+      <div style={{
+        textAlign: "center",
+        padding: "14px",
+        backgroundColor: "#f5f0eb",
+        borderBottom: "1px solid #ddd",
+        fontSize: "1rem",
+      }}>
+        Looking for this menu in English?{" "}
+        <Link
+          to="/menuspring"
+          style={{ fontWeight: "bold", textDecoration: "underline" }}
+        >
+          Click here for the English menu
+        </Link>
+      </div>
+
       {menuImages.map((img) => (
         <img
           key={img.src}
@@ -21,4 +40,3 @@ export default function Menu() {
     </div>
   );
 }
- 
