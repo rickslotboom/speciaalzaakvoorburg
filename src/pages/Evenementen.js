62287ade1b170b319e29f3ab2dc351484a6c5b33
Rick Slotboom <rick.slotboom@hotmail.com>
2026-06-05 10:14:55 +0200

    Evenementen aangepast2

--- src/pages/Evenementen.js
+++ src/pages/Evenementen.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Evenementen() {
   const eventImages = [
-     { src: "/images/poster1.png", alt: "Events mei – De Speciaalzaak" },
-    { src: "/images/poster2.png", alt: "Events juni – De Speciaalzaak" },
+     { src: "/images/poster1.jpeg", alt: "Events mei – De Speciaalzaak" },
+    { src: "/images/poster2.jpeg", alt: "Events juni – De Speciaalzaak" },
   ];
 
   return (
